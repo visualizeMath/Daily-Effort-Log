f50fc965a6dc3820705fc81d3161e033b1b5fc9c
ibrahim <ibrhim.odaci.1@gmail.com>
2025-07-31 18:38:21 +0300

    Prevented duplicate tasks creation

--- app.py
+++ app.py
@@ -203,17 +203,26 @@ def submit_pdas_task():
 
     conn = sqlite3.connect(db_path)
     c = conn.cursor()
-    c.execute('''INSERT INTO pdas (pdas_task_id, pdas_task_aciklama, bagli_sprint) 
+
+    c.execute(f'select pdas_task_id from pdas where pdas_task_id ={pdas_task_id} and bagli_sprint={bagli_sprint}')
+    sonuc=c.fetchone()
+    
+    print(f'Sonuc: {sonuc}')
+
+    if not sonuc:
+        c.execute('''INSERT INTO pdas (pdas_task_id, pdas_task_aciklama, bagli_sprint) 
                  VALUES (?, ?, ?)''', 
                  (pdas_task_id, pdas_task_aciklama, bagli_sprint))
-    conn.commit()
-    conn.close()
-
-    if c.rowcount == 0:
-        flash(f'PDAS kaydı kaydedilemedi.', 'danger')
-    flash(f'{pdas_task_id} - {pdas_task_aciklama} PDAS kaydı girildi.', 'success')
-
+        conn.commit()
+        conn.close()
+        flash(f'{pdas_task_id} - {pdas_task_aciklama} PDAS kaydı girildi.', 'success')
+        
+   
+    elif sonuc and int(sonuc[0])> 0:
+        flash(f'Aynı task numaralı başka bir kayıt var.', 'danger')
+    
     return redirect(url_for('create_task_bp.create_new_task'))
+
 
 
 #Find the downloads folder path depending on the os of the user
